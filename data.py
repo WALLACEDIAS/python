4d5f467032417c52149db1a9ce047066f342bcd8
WALLACEDIAS <36749717+WALLACEDIAS@users.noreply.github.com>
2019-05-09 12:46:12 -0300

    Add files via upload
    Formatando datas

---
+++ data.py
@@ -0,0 +1,14 @@
+
+class Data:
+
+    def __init__(self,dia,mes,ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
+    def formatada (self):
+       print("A data sendo formatada ficaria: {}/{}/{} !".format(self.dia,self.mes,self.ano))
+
+
+
+
